f4b596c3bfeadb2bb75268eeb3fcbc20d95a6cf0
jqka7970-sudo <jqka7970@gmail.com>
2026-02-10 06:31:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-const text = "WELCOME";
-const welcomeEl = document.getElementById("welcome");
-const startEl = document.getElementById("start");
+const startBtn = document.getElementById("startBtn");
 
-let index = 0;
-
-function typeText() {
-  if (index < text.length) {
-    welcomeEl.textContent += text[index];
-    index++;
-    setTimeout(typeText, 150);
-  } else {
-    setTimeout(() => {
-      startEl.classList.remove("hidden");
-    }, 600);
-  }
-}
-
-typeText();
-
-// 클릭 시 게임 페이지로 이동
-document.addEventListener("click", () => {
-  window.location.href = "game.html"; // 나중에 실제 게임 주소
+startBtn.addEventListener("click", () => {
+  // 나중에 실제 게임 주소로 변경
+  window.location.href = "game.html";
 });
+
